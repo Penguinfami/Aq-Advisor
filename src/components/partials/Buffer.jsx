fa02ed0e121f530d99beeb8cb65f19e0b6dfb655
Christine Zhao <christine.zhao@mail.utoronto.ca>
2022-01-04 15:06:11 -0500

    Create buffer component

---
+++ src/components/partials/Buffer.jsx
@@ -0,0 +1,15 @@
+import Button from './Button';
+
+const Buffer = () => {
+    return (
+        <div className="bufferScreen modal d-block">
+            <div className="bufferContent modal-content">
+                <div className="modal-body">
+                    Loading your stuff..
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Buffer
