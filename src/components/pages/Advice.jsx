2fbd4e8210de66f505f96d00b90c27017b38e253
Christine Zhao <christine.zhao@mail.utoronto.ca>
2022-01-04 13:42:42 -0500

    example advice for testing

--- src/components/pages/Advice.jsx
+++ src/components/pages/Advice.jsx
@@ -3,18 +3,43 @@ import Button from "../partials/Button"
 import { useNavigate } from 'react-router-dom';
 const Advice = (props) => {
 
+    const exampleAdvice = {
+
+        "warning" : [ 
+            {
+                "id" : 1,
+                "message" : "Raphael Catfish is not recommended for your tank - it may eventually outgrow your tank space, potentially reaching up to 7 inches."
+            }
+        ],
+        "suggestion" : [
+            {
+                "id" : 1,
+                "message" : "If you want to keep more than 1 Sunset Platy, minimum recommend male to female ratio is 1:2 (M:F). You will be less likely to experience problem if you get even more females."
+
+            }
+        ]
+    }
+
     const navigate = useNavigate();
 
     return (
         <div className="page">
             <Button onClick={() => navigate(props.prevPage)} title="Back" className="backButton"/>
             <TitleHeader/>
-            <div className="warning">
+            {exampleAdvice.warning ?
+             <div className="warning">
                 <h2>Warning!</h2>
-            </div>
-            <div className="suggestion">
+                {exampleAdvice.warning.map((warning) => <p className="warningMessage" key={warning.id}>{warning.message}</p>)}
+            </div>    
+            : null
+            }
+            {exampleAdvice.suggestion ?
+             <div className="suggestion">
                 <h2>Suggestion:</h2>
-            </div>
+                {exampleAdvice.suggestion.map((suggestion) => <p className="warningMessage" key={suggestion.id}>{suggestion.message}</p>)}
+            </div>    
+            : null
+            }
             <Button onClick={() => navigate(props.nextPage)}className="nextButton" title="Next" />
         </div>
     )
