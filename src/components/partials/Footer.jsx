a7c76da0f5d3c625eee485e5fdbb1dfc86430b45
Christine Zhao <christine.zhao@mail.utoronto.ca>
2022-01-06 17:03:06 -0500

    Added punctuation

--- src/components/partials/Footer.jsx
+++ src/components/partials/Footer.jsx
@@ -1,8 +1,8 @@
 const Footer = () => {
     return (
         <div className="footer">
-            <span>I do not own the AqAdvisor database.</span>
-            <span>Fish do not belong in a bowl, no matter what size. <a href="http://www.aqadvisor.com" target="_blank">www.aqadvisor.com</a> is a site for new and experienced fishkeepers to gauge their aquarium capacity and species recommendations.</span>
+            <span>Fish do not belong in a bowl, no matter what size! <a href="http://www.aqadvisor.com" target="_blank">www.aqadvisor.com</a> is a site for new and experienced fishkeepers to gauge their aquarium capacity and species recommendations. I do not own the AqAdvisor database.</span>
+
         </div>
     )
 }
