289204e3109c9af49e19f14fbc26a139e326b685
Christine Zhao <christine.zhao@mail.utoronto.ca>
2022-01-06 17:01:17 -0500

    Added more text to footer

--- src/components/partials/Footer.jsx
+++ src/components/partials/Footer.jsx
@@ -1,7 +1,8 @@
 const Footer = () => {
     return (
         <div className="footer">
-            <span>I do not own the AqAdvisor database. <a href="http://www.aqadvisor.com" target="_blank">www.aqadvisor.com</a> is a wonderful site for new and experienced fishkeepers to gauge their aquarium tank stocking capacity and recommendations.</span>
+            <span>I do not own the AqAdvisor database.</span>
+            <span>Fish do not belong in a bowl, no matter what size. <a href="http://www.aqadvisor.com" target="_blank">www.aqadvisor.com</a>is a site for new and experienced fishkeepers to gauge their aquarium capacity and species recommendations.</span>
         </div>
     )
 }
